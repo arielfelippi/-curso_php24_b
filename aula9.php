7b1ca600d89617cc03fa546989308541d3e3279e
arielfelippi <ariel.felippi@hotmail.com>
2024-08-13 22:04:32 -0300

    funcoes

--- aula9.php
+++ aula9.php
@@ -63,3 +63,25 @@ $valor = 10;
  * 
  */
 
+
+function banco($valor, $operacao, $saldoAtual) {
+
+    $saldo = $saldoAtual;
+
+    if ($operacao == "extrato") {
+        return $saldo;
+    }
+    
+    return $saldo;
+}
+
+$saldoAtual = 50;
+$valor = 100;
+$operacao = "deposito";
+
+$saldoAtual = banco($valor, $operacao, $saldoAtual);
+echo "Extrato: $valor $operacao = $saldoAtual<br>";
+
+$operacao = "saque";
+$saldoAtual = banco($valor, $operacao, $saldoAtual);
+echo "Extrato: $valor $operacao = $saldoAtual<br>";
