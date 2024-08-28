5018c0c7edb9a341a0b0043680827f0e2e2f9f31
arielfelippi <ariel.felippi@hotmail.com>
2024-08-27 21:43:53 -0300

    cpf cnpj mascaras

--- aula10.php
+++ aula10.php
@@ -2,7 +2,7 @@
 
 /**
  * Criar duas funcoes(separadas) onde cada uma recebe um parametro(string)
- * e retorna um valor boolean (true || false, 0 || 1).
+ * e retorna um valor boolean (true || false, 1 || 0).
  * 
  * Objetivo: Validar CPF, CNPJ
  * 
@@ -13,13 +13,63 @@
 */
 
 function isValidCPF($cpf) {
-    $padrao = "/^[0-9]*$/";
+    $padrao = "/[^0-9]/is";
     $cpf = preg_replace($padrao, "", $cpf);
 
     // codigo aqui
 
     return true;
 }
+
+/**
+ * CPF: ###.###.###-## = 14
+ * CPF: 12345678910 = 11
+ * CPF: 123.456.789-10 = 14
+ */
+function addMaskCPF($cpf) {
+    $cpfMascarado = "";
+    $maskCPF = "###.###.###-##"; // "491.582."
+
+    $a =0;
+    
+    for ($i=0; $i <= strlen($maskCPF); $i++) {
+
+        if ($maskCPF[$i] == '#') {
+            $cpfMascarado .=  $cpf[$a];
+            $a++;
+        } else {
+            $cpfMascarado .= $maskCPF[$i];
+        }
+
+    }
+
+    return $cpfMascarado;
+}
+
+$maskCPF = "###.###.###-##";
+$maskCNPJ = "##.###.###/####-##";
+
+addMask("12345678910", $maskCNPJ);
+
+function addMask($CpfCnpj, $mascara) {
+
+    $a =0;
+    $cpfMascarado = "";
+    
+    for ($i=0; $i <= strlen($mascara); $i++) {
+
+        if ($mascara[$i] == '#') {
+            $cpfMascarado .=  $CpfCnpj[$a];
+            $a++;
+        } else {
+            $cpfMascarado .= $mascara[$i];
+        }
+
+    }
+
+    return $cpfMascarado;
+}
+
 
 function isValidCNPJ($cnpj) {
     $padrao = "/^[0-9]*$/";
@@ -30,13 +80,21 @@ function isValidCNPJ($cnpj) {
     return true;
 }
 
-$cpf = "491.582.630-98";
+$cpf = "491.582.630-98aaa";
 
 
 $cpfValido = isValidCPF($cpf);
 
+$padrao = "/[^0-9]/is";
+$cpf = preg_replace($padrao, "", $cpf); // 49158263098 => 491.582.630-98
+
+
+
+
+
+
 if ($cpfValido) {
-    echo "O CPF: $cpf é Valido.";
+    echo "O CPF: $cpf é Valido."; // O CPF: 491.582.630-98aaa é válido.
 } else {
     echo "O CPF: $cpf é Invalido.";
 }
