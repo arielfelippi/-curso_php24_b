e913f9b93cb31bf7588337711a1879658dd1c4c9
arielfelippi <ariel.felippi@hotmail.com>
2024-10-08 21:29:41 -0300

    MVC e conexao banco

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # curso_php
 Aulas do curso de PHP da Escola Infoserv
 - Primeira aula dia 18-06-2024
+
+
+
+Patterns = padrão
+
+
+MVC
+
+Model = modelo = banco de dados
+
+View = visao/interface = telas usuario
+
+Controller = controlador = lógica/sistema
+
+
+CRUD -  operacoes (SQL)
+
+Create
+Read
+Update
+Delete
---
+++ src/models/UsuarioModel.php
@@ -0,0 +1,38 @@
+<?php
+
+// require './src/Conexao.php';
+
+class UsuarioModel {
+
+    private $fields = [
+        'id',
+        'cpf',
+        'email',
+        'senha',
+        'excluido',
+        'usuario_alteracao',
+        'data_criacao',
+        'data_atualizacao',
+    ];
+
+    public function create() {
+
+    }
+
+    public function read($id) {
+
+    }
+
+    public function readAll() {
+
+    }
+
+    public function update($id) {
+
+    }
+
+    public function delete($id) {
+
+    }
+
+}
